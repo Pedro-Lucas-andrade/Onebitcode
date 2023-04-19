74aa0f3f5289d0bc3ba7f175932f89bbc05c3e6b
Pedro-Lucas-andrade <pl314284@gmail.com>
2023-04-19 11:41:32 -0300

    ex

---
+++ JavaScript/ex002.js
@@ -0,0 +1,6 @@
+let pessoaVelha = prompt('Nome da pessoa mais velha: ')
+let idadeVelha = prompt('Idade da pessoa mais velha:')
+let pessoaNova = prompt('Nome da pessoa mais nova: ')
+let idadeNova = prompt('Idade da pessoa mais nova:')
+let difIdade = idadeVelha - idadeNova
+alert(`Nome: ${pessoaVelha} | Idade: ${idadeVelha} \n Nome: ${pessoaNova} | Idade: ${idadeNova} \n Diferença de idade: ${difIdade}`)
