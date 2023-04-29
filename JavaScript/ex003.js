137b2eb29ad4ddc33879bfe84a68ea12d9b9c607
Pedro-Lucas-andrade <pl314284@gmail.com>
2023-04-29 18:29:44 -0300

    novos exerccicios de javascript da onebitcode

---
+++ JavaScript/ex003.js
@@ -0,0 +1,7 @@
+let n1 = Number(prompt('Digite um número: '))
+let n2 = Number(prompt('Digite outro número: '))
+let soma = n1 + n2
+let sub = n1 - n2
+let mult = n1*n2
+let div = n1/n2
+alert(`Soma: ${soma}\nSubtração: ${sub}\nMultiplicação: ${mult}\nDivisão: ${div}`)
