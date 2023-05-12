54b404ab737a0aa60f420c6daba0defcfd9ad240
Pedro-Lucas-andrade <126332832+Pedro-Lucas-andrade@users.noreply.github.com>
2023-05-12 16:53:01 -0300

    exxercicos JS

---
+++ Exercicios_Apostila/Arrays/ex071.js
@@ -0,0 +1,8 @@
+const valor = 999
+const vetor = []
+let i = 0
+while ( i <= 7 ){
+    vetor[i] = valor
+    i++
+}
+console.log(vetor)
---
+++ Exercicios_Apostila/Arrays/ex072.js
@@ -0,0 +1,8 @@
+const vetor = []
+let i = 0
+let valor = 5
+do{
+    vetor[i] = 5*(i+1)
+    i++
+} while( i <= 9)
+console.log(vetor)
