a349890f3de479adacf63709263fb85f389d092c
Pedro-Lucas-andrade <126332832+Pedro-Lucas-andrade@users.noreply.github.com>
2023-05-31 15:28:49 -0300

    exercicios repetições

---
+++ Exercicios_Apostila/Repetições/ex046.js
@@ -0,0 +1,8 @@
+let i = 6;
+let soma = 0;
+while(i <= 100){
+    soma += i;
+    console.log(i + ' + ')
+    i += 2;
+}
+console.log(`A soma vale: ${soma}`);
---
+++ Exercicios_Apostila/Repetições/ex047.js
@@ -0,0 +1,8 @@
+let i = 500;
+let soma = 0;
+while( i >= 0){
+    console.log(i + ' + ');
+    soma += i;
+    i -= 50;
+}
+console.log('a soma vale ' + soma)
